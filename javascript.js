a4cb3dad5998fc7d74df8273751579e615ee416b
noireko <agustinfernandezrolon@gmail.com>
2025-06-11 13:33:49 -0300

    Added event listener to change color of the div when you hover with mouse

--- javascript.js
+++ javascript.js
@@ -2,8 +2,12 @@ const main_div = document.getElementById("main_div");
 
 for (div_child = 0; div_child < 256; div_child++) {
   const newDiv = document.createElement("div");
-  newDiv.style.backgroundColor = "red";
+  newDiv.style.backgroundColor = "white";
   newDiv.style.height = "50px";
   newDiv.style.width = "50px";
+  newDiv.style.border = "1px solid black";
   main_div.appendChild(newDiv);
+  newDiv.addEventListener("mouseover", function (event) {
+    event.target.style.backgroundColor = "black";
+  });
 }
